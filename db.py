ff6f995c1a106dec6cd8f899157631b0ff59057e
Oleg Popernyak <taktik838@gmail.com>
2020-10-25 23:55:26 +0200

    fix for pylint and flake8

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+'''Module for work with database'''
 import sqlite3
 import pickle
 
@@ -5,13 +6,17 @@ HOST = 'database.db'
 
 
 def connect_bd(fun):
+    '''Decorator for work with database
+    1st param is connect to db for fun'''
     def decorate(*args, **kwargs):
         with sqlite3.connect(HOST) as connect:
-            return fun(connect, *args, **kwargs)
+            return fun(*args, **kwargs, connect=connect)
     return decorate
 
+
 @connect_bd
-def _init_tables(connect):
+def _init_tables(connect=None):
+    '''Create table if it is not exists'''
     connect.execute('''CREATE TABLE IF NOT EXISTS area (
                     id INTEGER PRIMARY KEY NOT NULL,
                     points NOT NULL
@@ -23,56 +28,92 @@ def _init_tables(connect):
                     description
                     )''')
 
+
 @connect_bd
-def add_area(connect, points):
+def add_area(points, connect=None):
+    '''Add new area in db
+
+    Keyword arguments:
+    points -- points of area
+    '''
     connect.execute('INSERT INTO area VALUES(NULL, ?)', [pickle.dumps(points)])
     connect.commit()
-    
+
+
 @connect_bd
-def add_areas(connect, areas):
+def add_areas(areas, connect=None):
+    '''Add new areas in db in one time
+
+    Keyword arguments:
+    areas -- list of points of areas
+    '''
     dump_areas = [(pickle.dumps(points),) for points in areas]
     connect.executemany('INSERT INTO area VALUES(NULL, ?)', dump_areas)
     connect.commit()
-    
+
+
 @connect_bd
-def get_all_areas(connect):
+def get_all_areas(connect=None):
+    '''Return list of (id, points) of all areas'''
     response = connect.execute('SELECT * FROM area').fetchall()
     response = list(map(lambda row: (row[0], pickle.loads(row[1])), response))
     return response
 
-@connect_bd
-def get_area(connect, id_area):
-    res = connect.execute('SELECT * FROM area WHERE id=?', [id_area]).fetchall()
-    if res:
-        return res[0]
-    else:
-        raise sqlite3.DataError(f'Area with id={id_area} is not exists')
 
 @connect_bd
-def add_courier(connect, id_area, description):
-    area = get_area(id_area)
-    connect.execute('INSERT INTO courier VALUES(NULL, ?, ?)', [id_area, description])
+def get_area(id_area, connect=None):
+    '''Return area(id, points) by id'''
+    res = connect.execute('SELECT * FROM area WHERE id=?', [id_area])
+    res = res.fetchall()
+    if not res:
+        raise sqlite3.DataError(f'Area with id={id_area} is not exists')
+    return res[0]
+
+
+@connect_bd
+def add_courier(id_area, description, connect=None):
+    '''Add new courier
+
+    Keyword arguments:
+    id_area -- area will be assigned to courier
+    description -- info about courier
+    '''
+    # check there is area with id_area
+    get_area(id_area)
+    connect.execute('INSERT INTO courier VALUES(NULL, ?, ?)',
+                    [id_area, description])
     connect.commit()
-    
+
+
 @connect_bd
-def get_couriers_by_area(connect, id_area):
-    res = connect.execute('SELECT * FROM courier WHERE id=?', [id_area]).fetchall()
-    if res:
-        return res
-    else:
-        if not get_area(id_area):
-            raise sqlite3.DataError(f'There are not couriers at this area({id_area})')
-        
+def get_couriers_by_area(id_area, connect=None):
+    '''Return couriers(list of (id_courier, id_area, description))
+    who assigned to id_area'''
+    res = connect.execute('SELECT * FROM courier WHERE id=?', [id_area])
+    res = res.fetchall()
+    if not res and not get_area(id_area):
+        raise sqlite3.DataError(
+            f'There are not couriers at this area({id_area})'
+        )
+    return res
+
+
 @connect_bd
-def get_all_couriers(connect):
+def get_all_couriers(connect=None):
+    '''Return all couriers(list of (id_courier, id_area, description))'''
     return connect.execute('SELECT * FROM courier').fetchall()
 
+
 @connect_bd
-def clear_areas(connect):
+def clear_areas(connect=None):
+    '''Delete all areas'''
     connect.execute('DELETE FROM area')
-    
+
+
 @connect_bd
-def clear_couriers(connect):
+def clear_couriers(connect=None):
+    '''Delete all couriers'''
     connect.execute('DELETE FROM courier')
+
 
 _init_tables()
--- delivery_area.py
+++ delivery_area.py
@@ -1,41 +1,82 @@
+'''API for managment to area'''
 import db
 
+
 def get_id_area(coor_place):
+    '''Return id of area who have coor_place'''
     areas = db.get_all_areas()
     for id_area, points in areas:
         if _point_in_area(coor_place, points):
             return id_area
+    return None
+
 
 def get_all_areas():
+    '''Return list of (id, points) of all areas'''
     return db.get_all_areas()
 
+
 def add_area(points):
+    '''Add new area in db
+
+    Keyword arguments:
+    points -- points of area
+    '''
     db.add_area(points)
-    
+
+
 def add_areas(area_points):
+    '''Add new areas in db in one time
+
+    Keyword arguments:
+    area_points -- list of points of areas
+    '''
     db.add_areas(area_points)
 
+
 def get_area_points(id_area):
+    '''Return area(id, points) by id'''
     return db.get_area(id_area)
 
+
 def assign_courier_to_area(courier_description, id_area):
+    '''Assign new courier to area
+
+    Keyword arguments:
+    courier_description -- info about courier
+    id_area -- area will be assigned to courier
+    '''
     db.add_courier(id_area, courier_description)
-    
+
+
 def assign_courier_for_delivery(coord_delivery):
+    '''Assign courier to delivery
+
+    Keyword arguments:
+    coord_delivery -- coordinates of place delivery
+    '''
     id_area = get_id_area(coord_delivery)
     courier = db.get_couriers_by_area(id_area)
     courier_description = courier[0][2]
     return id_area, courier_description
 
+
 def _point_in_area(point, area_points):
-    X, Y = point
+    '''Return true if point at area else false
+
+    Keyword arguments:
+    point -- coordinates of needed point
+    area_points -- coordinates of chosen area
+    '''
+    x_point, y_point = point
     result = False
-    j = len(area_points) - 1
-    for i in range(len(area_points)):
-        x1, y1 = area_points[i]
-        x2, y2 = area_points[j]
-        j = i
-        if ( (y1 < Y and y2 >= Y or y2 < Y and y1 >= Y) and
-                (x1 + (Y - y1) / (y2 - y1) * (x2 - x1) < X) ):
+    x2_area, y2_area = area_points[-1]
+    for x1_area, y1_area in area_points:
+        if ((y1_area < y_point <= y2_area or
+            y2_area < y_point <= y1_area) and
+                (x1_area + (y_point - y1_area) / (y2_area - y1_area) *
+                 (x2_area - x1_area) < x_point)):
             result = not result
+
+        x2_area, y2_area = x1_area, y1_area
     return result
